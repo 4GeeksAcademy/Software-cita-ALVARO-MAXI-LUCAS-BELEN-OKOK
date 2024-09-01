cdb2e903d3bdcd810d76d653814c1ee3c28f1ae1
lucki200 <90269862+lucki200@users.noreply.github.com>
2024-09-01 10:27:34 +0000

    componentes y extensiones

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,11 +8,13 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+			<h1>Bienvenido a Nuestra Clinica</h1>
 			<p>
 				<img src={rigoImageUrl} />
+				Ofrecemos los mejores servicios medicos para el cuidado de tus ojos
 			</p>
 			<div className="alert alert-info">
+				
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
 			<p>
