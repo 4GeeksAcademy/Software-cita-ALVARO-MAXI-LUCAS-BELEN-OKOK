19dd9235859e0cad3527aabaf8d9fcdd2096ed5e
MariaBelenGallardoPedretti <belengallardop@gmail.com>
2024-09-11 19:33:51 +0000

    service modificado

--- src/front/js/pages/services.js
+++ src/front/js/pages/services.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 export const Services = () => {
   return (
-    <Container className="mt-4">
+    <Container className="mt-4 text-center">
       <h2>Nuestros Servicios</h2>
       <Row>
         <Col md={4}>
@@ -12,7 +12,9 @@ export const Services = () => {
             <Card.Body>
               <Card.Title>Exámenes de Vista</Card.Title>
               <Card.Text>
-                Realizamos exámenes exhaustivos para evaluar tu salud ocular.
+                Realizamos exámenes exhaustivos para evaluar tu salud ocular y te proporcionamos la receta necesaria para que puedas acceder a tus lentes con graduación de acuerdo a la patología que tengas.
+                <br/>
+                ¡Pudiendo acceder a los mismos dentro de la clínica!
               </Card.Text>
               <Link to="/services/1">
                 <Button variant="primary">Más Información</Button>
@@ -25,7 +27,9 @@ export const Services = () => {
             <Card.Body>
               <Card.Title>Cirugías</Card.Title>
               <Card.Text>
-                Ofrecemos cirugías especializadas para tratar diversas condiciones.
+                Contamos con profesionales de alto nivel que estarán contigo en todo momento para aclarar tus dudas y hacer el acompañamiento necesario previo, durante y después de la cirigía.
+                <br/>
+                Cada una de nuestras cirugías, cuenta con los mejores especialistas para cada condición ocular.
               </Card.Text>
               <Link to="/services/2">
                 <Button variant="primary">Más Información</Button>
