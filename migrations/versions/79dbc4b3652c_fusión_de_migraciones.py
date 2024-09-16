a8f96a063496eda688fec2fc022c56837ecd5702
MariaBelenGallardoPedretti <belengallardop@gmail.com>
2024-09-16 19:07:23 +0000

    nuevo repo

---
+++ migrations/versions/79dbc4b3652c_fusión_de_migraciones.py
@@ -0,0 +1,24 @@
+"""Fusión de migraciones
+
+Revision ID: 79dbc4b3652c
+Revises: 
+Create Date: 2024-09-16 18:03:51.496782
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '79dbc4b3652c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
